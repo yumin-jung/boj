cbfad9beb703819c3402818fba6e65f0419c57f5
yumin-jung <jungym887@naver.com>
2024-09-24 16:22:58 +0900

    node.js 1463

---
+++ 1463.js
@@ -0,0 +1,22 @@
+const input = require('fs')
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+    .toString()
+    .trim();
+
+const N = Number(input);
+
+const dp = Array(N + 1).fill(0);
+
+for (let i = 2; i <= N; i++) {
+    dp[i] = dp[i - 1] + 1;
+
+    if (i % 3 === 0) {
+        dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    }
+
+    if (i % 2 === 0) {
+        dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    }
+}
+
+console.log(dp[N]);
