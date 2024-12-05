a6a37d0e02b8ef008f344d18dfeec5110ed5ea0c
yumin-jung <jungym887@naver.com>
2024-12-06 04:06:18 +0900

    node.js 11726

---
+++ node/11726.js
@@ -0,0 +1,13 @@
+const input = require('fs')
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./dev/input.txt")
+    .toString()
+    .trim();
+
+const n = Number(input);
+const dp = [1, 2];
+
+for (let i = 2; i < n; i++) {
+    dp.push((dp[i - 1] + dp[i - 2]) % 10007);
+}
+
+console.log(dp[n - 1]);
