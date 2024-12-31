1c4f34fc1368804a544719666315ae62ad80b0b8
yumin-jung <jungym887@naver.com>
2024-12-31 23:56:53 +0900

    cpp 2750

---
+++ cpp/2750.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int arr[1001];
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
