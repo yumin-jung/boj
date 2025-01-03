4e5fec2a3d3882d569e4aee6d17cc70ef2b64f46
yumin-jung <jungym887@naver.com>
2025-01-04 00:17:02 +0900

    cpp 15652

---
+++ cpp/15652.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[9];
+
+void dfs(int n, int k) {
+    if(k == M) {
+        for(int i = 0; i < M; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    } else {
+        for (int i = n; i <= N; i++) {
+            arr[k] = i;
+            dfs(i, k + 1);
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    dfs(1, 0);
+}
